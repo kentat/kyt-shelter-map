c985b51817d022c5631261fb48c02f398ae0ba7e
kentaro takahashi <takahashi.kentaro@gmail.com>
2021-09-20 12:49:25 +0900

    a

--- bosai/views.py
+++ bosai/views.py
@@ -1,16 +1,12 @@
-#from django.views.generic import TemplateView
 from bosai.models import ShelterModel
-#from bosai.forms import ShelterModelForm
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
-from django.core import serializers
 
 # Create your views here.
 def index(request):
-    tmp_list = ShelterModel.objects.all()
-    # return render(request, 'bosai/list.html')
-    # return render(request, 'bosai/list.html', {'shelters': tmp_list})
-    return render(request, 'bosai/index.html', {'shelters': tmp_list})
-#def list(request):
-#    shelter = ShelterModel()
-#    return render(request, 'bosai/list.html', {'shelters':shelter})
+    list = ShelterModel.objects.all()
+    return render(request, 'bosai/index.html', {'shelters': list})
+    # return HttpResponse("You're looking at question .")
+def detail(request, id):
+    shelter = get_object_or_404( ShelterModel, pk=id)
+    return render(request, 'bosai/detail.html', {'shelter':shelter})
