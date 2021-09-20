a7a4d1640901f05e57451d89e6316c872f1effbc
kentaro takahashi <takahashi.kentaro@gmail.com>
2021-09-21 06:11:47 +0900

    add div options

--- bosai/views.py
+++ bosai/views.py
@@ -4,8 +4,8 @@ from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
 def index(request):
-    list = ShelterModel.objects.all()
-    return render(request, 'bosai/index.html', {'shelters': list})
+    shs = ShelterModel.objects.all()
+    return render(request, 'bosai/index.html', {'shelters': shs})
     # return HttpResponse("You're looking at question .")
 def detail(request, id):
     shelter = get_object_or_404( ShelterModel, pk=id)
